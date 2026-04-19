22e965406121528b907101e32c6f99f732551c17
mary-emm <marcygump06@gmail.com>
2026-04-19 21:10:08 +0800

    test deployment -mm-

--- src/examples/02example.svelte
+++ src/examples/02example.svelte
@@ -1,26 +1,28 @@
-<!-- <script>
-    let count = $state(0)
-</script>
-
-<button
-	class="bg-[#FFA23A] text-[#111111] px-4 py-2 rounded"
-	onclick={() => count++}
->
-	Clicked {count} times
-</button> -->
-
-
 <!-- logic -->
 
 <script>
-    // specifying that the count variable is reactive
-    let count = $state(0)
-
+  // specifying that the count variable is reactive
+  let count = $state(0);
 </script>
 
-<button
-class = "bg-[#FFA23A] text-[#111111] px-4 py-2 rounded-lg"
+<div class="flex flex-col gap-4 items-center">
+  <p>Count is {count}</p>
 
- onclick={() => count += 1}>
-This button was clicked {count} times    
-</button>
+  <div>
+    <button
+      class="bg-[#FFA23A] text-[#111111] px-4 py-2 rounded-lg"
+      onclick={() => (count += 1)}
+    >
+      +
+    </button>
+
+    <button
+      class="bg-[#FFA23A] text-[#111111] px-4 py-2 rounded-lg"
+      onclick={() => (count -= 1)}
+    >
+      -
+    </button>
+  </div>
+</div>
+
+<!-- <div class="flex flex-col gap-4"></div> -->
